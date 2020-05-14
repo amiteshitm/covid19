b7763a165e0d002331f812076719bb1e39062cd7
amiteshitm <amiteshitm2011@gmail.com>
2020-05-15 00:19:48 +0530

    Updates

---
+++ precache-manifest.dbbd5c3a880d62fa93899818a585ea7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79de95b0725cb1c2366b75c354380f98",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "44177724bd9ecba0a418",
+    "url": "/covid-19/static/css/main.4ce65777.chunk.css"
+  },
+  {
+    "revision": "026c712f53a3a1a1fbd1",
+    "url": "/covid-19/static/js/2.99ea17e3.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.99ea17e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44177724bd9ecba0a418",
+    "url": "/covid-19/static/js/main.8ad894ad.chunk.js"
+  },
+  {
+    "revision": "af427c12718010fc97b8",
+    "url": "/covid-19/static/js/runtime-main.4e6896ff.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
